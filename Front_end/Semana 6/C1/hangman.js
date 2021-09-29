3f80a1b5bc87c9d6636ababb74801f85a673149d
Gustavo Yupanqui <20111383@aloe.ulima.edu.pe>
2021-09-28 19:25:35 -0500

    actualizacion letras

--- Front_end/Semana 6/C1/hangman.js
+++ Front_end/Semana 6/C1/hangman.js
@@ -3,6 +3,10 @@ const refranes = [
     "CAMARON QUE SE DUERME SE LO LLEVA LA CORRIENTE",
     "QUIEN HIERRO MATA A HIERRO MUERE"
 ]
+
+var refran = ""
+var refranOculto = ""
+
 const elegirRefran = () =>{
     const pos = Math.round(Math.random()*2);
     return refranes[pos]
@@ -27,11 +31,39 @@ const cargarRefran = (refran) =>{
     divRefran.innerText = refran
 }
 
+const buscarLetraRefran = (letra, refran, refranOculto) =>{
+    let nuevoRefranOculto = ""
+    for(let i=0; i < refran.length; i++){
+        if(letra == refran[i]){
+            //se encuentra la letra
+            nuevoRefranOculto += refran[i]
+        }else{
+            nuevoRefranOculto += refranOculto[i]
+        }
+    }
+    return nuevoRefranOculto;
+}
+
+const letraInputOnkeypress = (evt) =>{
+    const letraIngresada = evt.key.toUpperCase()
+    const nuevoRefranOculto = buscarLetraRefran(letraIngresada, refran, refranOculto)
+    if(refranOculto == nuevoRefranOculto){
+        //no encontro una letra
+        console.log("deberia mostrar una imagen")
+    }else{
+        refranOculto = nuevoRefranOculto
+        cargarRefran(refranOculto)
+    }
+}
+
 const main = () =>{
-    const refran = elegirRefran()
-    const refranOculto = ocultarRefran(refran)
+    refran = elegirRefran()
+    refranOculto = ocultarRefran(refran)
     
     cargarRefran(refranOculto)
+
+    let inputLetras = document.querySelector("#letras")
+    inputLetras.addEventListener("keypress", letraInputOnkeypress)
 }
 
 window.addEventListener("load",main)
