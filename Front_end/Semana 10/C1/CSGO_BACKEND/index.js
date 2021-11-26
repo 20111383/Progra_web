532ae6a1f6c15cf904e1664f01cfc12697fbf57f
Gustavo Yupanqui <20111383@aloe.ulima.edu.pe>
2021-11-26 16:49:22 -0500

    formulario + template / CSGO + template 20/OCT

--- Front_end/Semana 10/C1/CSGO_BACKEND/index.js
+++ Front_end/Semana 10/C1/CSGO_BACKEND/index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
 const PORT = 5000
-const app = express()
+const app = express() 
 
 app.use(express.static('assets')) //soporte de archivos estaticos
-app.set('view engine', 'ejs')//configuramos el motor de template
+app.set('view engine', 'ejs') //configuramos el motor de templates
 
-//Endpoints
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.render('index')
 })
-app.listen(PORT,() => {
+
+app.listen(PORT, () => {
     console.log(`El servidor se inicio correctamente en el puerto ${PORT}`)
 })
